abf994ca9f0d227e710b8cec6a49a9c0b0681534
Thiago Vieira Pereira <th91vi@gmail.com>
2021-04-26 22:12:35 -0300

    feat: added routes, responses in express

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, message: 'Show all bootcamps'})
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Display bootcamp #${req.params.id}`})
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({success: true, message: 'Created new bootcamp'})
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Created bootcamp #${req.params.id}`})
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({success: true, message: `Deleted bootcamp (was #${req.params.id})`})
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
