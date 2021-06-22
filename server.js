b1aec6e324768f8a13328881ca871f139e3a2528
Thiago Vieira Pereira <th91vi@gmail.com>
2021-06-22 20:16:23 -0300

    feat: added mongoose. stablished connection to DB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -16,7 +19,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on("unhandledRejection", (error, promisse) => {
+  console.log(`Error: ${error.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
