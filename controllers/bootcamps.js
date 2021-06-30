7d0f4fc74590c9ae53f69f25205bcc87992c6f84
Thiago Vieira Pereira <th91vi@gmail.com>
2021-06-29 21:46:41 -0300

    feat: added GET methods for bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,35 @@
 const Bootcamp = require("../models/Bootcamp");
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({
+      success: true,
+      data: bootcamps,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Display bootcamp #${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No bootcamp found for given id" });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 exports.createBootcamp = async (req, res, next) => {
