e3aafc3944c8c6bdbdf88267b6bcd512cecfeca1
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-23 16:17:48 -0300

    feat: added user register method. password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,5 +3,18 @@ const User = require("../models/User");
 const asyncHandler = require("../middleware/async");
 
 exports.registerUser = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res.status(200).json({
+    succes: true,
+    data: user,
+  });
 });
