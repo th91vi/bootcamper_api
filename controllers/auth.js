5bd5b11d542847cb54728237a82f653a4f8fad8e
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-23 17:33:12 -0300

    feat: added user login method

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,3 +26,38 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
     },
   });
 });
+
+exports.loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 401));
+  }
+
+  // Check for user
+  const user = await User.findOne({
+    email,
+  }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if user entered password matches to hashed password in database
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    succes: true,
+    data: {
+      token,
+    },
+  });
+});
