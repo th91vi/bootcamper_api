5436b704096b6bc6fb07b515d47ede11a84d91b7
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-26 17:17:20 -0300

    feat: added relationship between user and bootcamp, at bootcamp creation

--- seeder.js
+++ seeder.js
@@ -11,6 +11,7 @@ dotenv.config({
 // Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,11 +30,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log("Data imported successfully".green.inverse);
     process.exit();
@@ -47,6 +53,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log("All data destroyed successfully".green.inverse);
     process.exit();
