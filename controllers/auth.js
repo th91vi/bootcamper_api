e71e1897ed87c9bea62364a86f9ceb42fea7880a
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-27 12:19:33 -0300

    feat: added method to send email with reset token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
+const sendEmail = require("../utils/sendEmail");
 const asyncHandler = require("../middleware/async");
 
 exports.registerUser = asyncHandler(async (req, res, next) => {
@@ -90,8 +91,31 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     validateBeforeSave: false,
   });
 
-  res.status(200).json({
-    sucess: true,
-    data: user,
-  });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subejct: "Password reset token",
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: "Email sent",
+    });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
 });
