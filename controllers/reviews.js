2a9fa67efac94d62834ff3f98972f0fde53c5e26
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-28 21:14:50 -0300

    feat: added method to create review for given bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -29,3 +29,20 @@ exports.getSingleReview = asyncHandler(async (req, res, next) => {
 
   return res.status(200).json({ success: true, data: review });
 });
+
+exports.createReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`, 404)
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  return res.status(200).json({ success: true, data: review });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { getReviews, getSingleReview } = require("../controllers/reviews");
+const {
+  getReviews,
+  getSingleReview,
+  createReview,
+} = require("../controllers/reviews");
 
 const Review = require("../models/Review");
 
@@ -8,13 +12,16 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middleware/advancedResults");
 const { protect, authorizeRoles } = require("../middleware/auth");
 
-router.route("/").get(
-  advancedResults(Review, {
-    path: "bootcamp",
-    select: "name text",
-  }),
-  getReviews
-);
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp",
+      select: "name text",
+    }),
+    getReviews
+  )
+  .post(protect, authorizeRoles("user", "admin"), createReview);
 router.route("/:id").get(getSingleReview);
 
 module.exports = router;
